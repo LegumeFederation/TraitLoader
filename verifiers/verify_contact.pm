5abe6eabdecf6716a44ff1e94b1fbe427a016e8d
Vijay Karingula <vk2@lis-dev.agron.iastate.edu>
2015-12-07 16:18:31 -0600

    changes according to new key format in key->value

--- verifiers/verify_contact.pm
+++ verifiers/verify_contact.pm
@@ -31,13 +31,14 @@ sub verify_contact {
   my ($column_name, $row_num, $berkeley_key, $berkeley_value);
   my $cursor = $berkeley_dbh->db_cursor();
   while ($cursor->c_get($berkeley_key, $berkeley_value, DB_NEXT) == 0) {
+  #splitting the $berkeley_key which is in the format (sheetname:colname:rownum)
   my @details = split(/:/,$berkeley_key);
-  $column_name = $details[0];    
+  $column_name = $details[1];    
   $row_num = $details[2];  
      
       switch($berkeley_key){
         
-        case (/^[name_code]:[.*]/){
+        case (/^[.*]:[name_code]:[.*]/){
           #RULE: name_code must be filled in.
           if (!$berkeley_value) {
             $error_count++;
@@ -45,7 +46,7 @@ sub verify_contact {
           }#if
         }#case: name_code
         
-        case (/^[name]:[.*]/){
+        case (/^[.*]:[name]:[.*]/){
           #RULE: name must be filled in.
           if (!$berkeley_value) {
             $error_count++;
@@ -59,12 +60,12 @@ sub verify_contact {
           }#if
         }#case: name
         
-        case (/^[contact]:[.*]/) {
+        case (/^[.*]:[contact]:[.*]/) {
           $error_count++;
           print "ERROR in the record $row_num. The value in the column $column_name is missing \n";        
         }#case: contact
         
-        case (/^[contact_type]:[.*]/){
+        case (/^[.*]:[contact_type]:[.*]/){
           #RULE: contact_type field must be filled-in
           if (!$berkeley_value) {
             $error_count++;
@@ -83,46 +84,6 @@ sub verify_contact {
         
       }#switch 
   }#while - For each record of contact sheet
-  
-  
- ################ --------ETHY's Code------################## 
-  #foreach my $ctr (sort { $a <=> $b } keys %{$excel_ref}) {
-  #
-  #  # RULE: fields name_code, name, and contact must be filled in
-  #  # -----
-  #  if (!$excel_ref->{$ctr}{name_code}) {
-  #    $error_count++;
-  #    print "ERROR in row $excel_ref->{$ctr}{row}: missing required 'name_code' field in record $ctr\n";
-  #  }
-  #  if (!$excel_ref->{$ctr}{name}) {
-  #    $error_count++;
-  #    print "ERROR in row $excel_ref->{$ctr}{row}: missing required 'name' field in record $ctr\n";
-  #  }
-  #  if (!$excel_ref->{$ctr}{contact_type}) {
-  #    $error_count++;
-  #    print "ERROR in row $excel_ref->{$ctr}{row}: missing required 'contact_type' field in record $ctr\n";
-  #  }
-  #  
-  #  # RULE: contact_type must be in trait dictionary
-  #  # -----
-  #  my $contact_type = $excel_ref->{$ctr}{contact_type};
-  #  my $contact_type_id = get_cvterm_id($contact_type, $data_info_ref->{CV_ID});
-  #  if (!$contact_type_id) {
-  #    $error_count++;
-  #    print "ERROR in row $excel_ref->{$ctr}{row}: no contact type '$contact_type' is in the trait dictionary.\n";
-  #    print "      Permitted contact types include: " . join(', ', keys(get_contact_types($pg_db)));
-  #    print "\n";
-  #  }
-  #
-  #  # RULE: warn if name/name_code already in contact 
-  #  # -----
-  #  if (get_contact($pg_db, $excel_ref->{$ctr}{name})) {
-  #    $warning_count++;
-  #    print "warning in row $excel_ref->{$ctr}{row}: a contact record with the ";
-  #    print "name '$excel_ref->{$ctr}{name}' already exists.\n";
-  #  }
-  #}#each contact record
-#############################################################
 
   if ($error_count == 0 && $warning_count > 0) {
     my $prompt = "There are $warning_count warnings in the contact worksheet. ";
